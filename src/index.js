34ade86e517bdb1ec4f28826287994a16b59a52b
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-07-20 23:43:41 +0200

    added react-facebook provider

--- src/index.js
+++ src/index.js
@@ -2,28 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import {unregister as unregisterServiceWorker} from './registerServiceWorker';
 import * as firebase from 'firebase';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-window.firebase = firebase;
+import FacebookProvider from 'react-facebook';
 
 injectTapEventPlugin();
 
-var config = {
+firebase.initializeApp({
   apiKey: "AIzaSyCS3sfQGzcHhs4iiNNaIoO1Xp7E98paBA4",
   authDomain: "enjam-coffee.firebaseapp.com",
   databaseURL: "https://enjam-coffee.firebaseio.com",
   projectId: "enjam-coffee",
   storageBucket: "enjam-coffee.appspot.com",
   messagingSenderId: "1049251801594"
-};
-firebase.initializeApp(config);
+});
+window.firebase = firebase;
 
 ReactDOM.render(
   <MuiThemeProvider>
-    <App/>
+    <FacebookProvider appId="1311032879006324" version="v2.10">
+      <App/>
+    </FacebookProvider>
   </MuiThemeProvider>
   , document.getElementById('root'));
-//registerServiceWorker();
+  
+unregisterServiceWorker();

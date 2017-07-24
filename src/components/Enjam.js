53863d70fd58b49be169aa14bdcb7f13710b4a48
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-07-24 22:48:03 +0200

    added enjam, iotjam descriptions

--- src/components/Enjam.js
+++ src/components/Enjam.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import {Page} from 'react-facebook';
+import Paper from 'material-ui/Paper';
 
 class Enjam extends Component {
   render() {
     return (
-      <div className="flex-center margin-body">
+      <div className="flex-center margin-body flex-column">
         <Page
           href="https://www.facebook.com/enjam.sdu/"
           tabs="none" 
           width={window.innerWidth - 32}
         />
+        <br/>
+        <Paper className="page" zDepth={0} style={{backgroundColor: 'none', maxWidth: '500px'}}>
+          Enjam arrangerer ingeniør-jams for studerende på SDU. 
+          Idéen er at skabe et kreativt miljø, der sætter de 
+          studerendes fagligheder og interesser i spil på tværs af uddannelserne.
+          <br/><br/>
+          Hver jam tager afsæt i et tema med udfordringer, 
+          der er relevante for en række ingeniørudannelser. 
+          Deltagerne finder sammen i mindre grupper og har weekenden til at lave et produkt.
+          <br/><br/>
+          Deltagerne skulle gerne tage hjem med inspiration, 
+          eventuelle præmier og et større netværk.
+        </Paper>
+        <br/><br/>
       </div>
     );
   }
--- src/components/Iotjam.js
+++ src/components/Iotjam.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import {EmbeddedPost} from 'react-facebook';
+import Paper from 'material-ui/Paper';
 
 class Iotjam extends Component {
   render() {    
     return (
-      <div className="flex-center margin-body fb-post-container-parent">
-        <EmbeddedPost 
-          href="https://www.facebook.com/events/1748049711875959/permalink/1748055881875342/"
-          width={window.innerWidth - 32}
+      <div className="flex-center margin-body flex-column">
+        <div className="fb-post-container-parent">
+          <EmbeddedPost 
+            href="https://www.facebook.com/events/1748049711875959/permalink/1748055881875342/"
+            width={window.innerWidth - 32}
           />
+        </div>
+        <br/>
+        <Paper className="page" zDepth={0} style={{backgroundColor: 'none', maxWidth: '750px'}}>
+          Er du interesseret i at udvikle fremtidens 'smarte' byer, 
+          en 'smart' industri, et 'smart' hjem eller noget andet 'smart'?
+          <br/><br/>
+          Kom og red verden - eller lav noget skørt - 
+          til Internet of Things jam på TEK, Syddansk Universitet. 
+          <br/>
+          Team op med nogle der komplementerer dine interesser og færdigheder. 
+          I har weekenden til at lave et IoT-koncept og måske en prototype. 
+          Det kan være, du vil automatisere din urtehave, 
+          sætte en gps på din ekskærestes cykel eller lave flotte grafer over 
+          temperaturen i dit køleskab.
+          <br/><br/>
+          Der vil være et kort foredrag om IoT, så du ikke behøver at kende til IoT i forvejen. 
+          Til prototypeudvikling er der adgang til 3D-printere, en laserskærer, 
+          IoT development boards, sensorer, aktuatorer og en hjælpende hånd.
+          <br/><br/>
+          Du får mest ud af arrangementet, hvis du har sat hele weekenden af. 
+          Fredag dannes der grupper og der idégenereres, lørdag udvikles der i fuld fart, 
+          og søndag præsenterer og vurderer i jeres produkter. 
+          Søndag eftermiddag sluttes der af med præmieoverrækkelser.
+        </Paper>
+        <br/><br/>
       </div>
     );
   }
